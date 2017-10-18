88afe166db337613c8ac9026566c2adcf743b9f7
MacRayy <mecrai@gmail.com>
2017-10-18 12:07:53 +0200

    chore(eslint): linter added to project

--- awesomeapp/src/App.js
+++ awesomeapp/src/App.js
@@ -1,11 +1,21 @@
-/*jshint esversion: 6 */
 'use strict'
 
 import React from 'react'
 
+// class App extends React.Component {
+// 	render() {
+// 		return React.createElement ('h1', null, 'hello world')
+// 	}
+// }
+
 class App extends React.Component {
 	render() {
-		return React.createElement ('h1', null, 'hello world')
+		return (
+			<div>
+				<h1>Hello hello</h1>
+				<span>world</span>
+			</div>
+		)
 	}
 }
 
