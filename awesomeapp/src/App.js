252ef8059142b786da86e274cb123757c3dd1605
MacRayy <mecrai@gmail.com>
2017-10-18 14:06:15 +0200

    feat(app): passing props between modules

--- awesomeapp/src/App.js
+++ awesomeapp/src/App.js
@@ -10,13 +10,21 @@ import React from 'react'
 
 class App extends React.Component {
 	render() {
+		let txt = this.props.txt
+		let cat = this.props.cat
+
 		return (
 			<div>
-				<h1>Hello hello</h1>
-				<span>world</span>
+				<h1>{txt}</h1>
+				<p>{cat}</p>
+				<p>{this.props.fruit}</p>
 			</div>
 		)
 	}
+}
+
+App.defaultProps = {
+	txt: 'this is the default text'
 }
 
 export default App
--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(
-	<App />,
+	<App cat="alma" fruit="korte"/>,
 	document.getElementById('root')
 )
